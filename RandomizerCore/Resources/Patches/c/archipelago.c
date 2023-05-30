3f17932a6777adbad1aace0eab158ab286163e19
Cameron Wong <cam@camdar.io>
2023-05-30 08:46:49 -0400

    initial archipelago stuff
    Signed-off-by: Cameron Wong <cam@camdar.io>

---
+++ RandomizerCore/Resources/Patches/c/archipelago.c
@@ -0,0 +1,34 @@
+#include "global.h"
+#include "item.h"
+
+struct ArchipelagoReceivedItem
+{
+    Item id;                     // The item to be received
+    u16 itemIndex;               // The index of the item according to the AP server
+    bool8 isFilled;              // Whether there is an item in this struct that has not been consumed
+    bool8 isProgression;         // Whether a message is progression (for filtering messages)
+};
+
+extern void InitItemGetSequence(u32 type, u32 type2, u32 delay);
+
+struct ArchipelagoReceivedItem *gArchipelagoReceivedItem = (struct ArchipelagoReceivedItem *) 0x02033400;
+
+inline void ArchipelagoTryGiveItem() {
+    if (gArchipelagoReceivedItem->isFilled) {
+        // TODO: Kinstones
+        InitItemGetSequence(gArchipelagoReceivedItem->itemIndex, 0, 0);
+        gArchipelagoReceivedItem->isFilled = FALSE;
+    }
+}
+
+extern void UpdateDoorTransition(void);
+extern void CollisionMain(void);
+extern void UpdateScroll();
+
+// Hook in [archipelago.event] takes 6 words, wiping 3 bl calls
+void ArchipelagoTryGiveItem_Hook() {
+    ArchipelagoTryGiveItem();
+    UpdateDoorTransition();
+    CollisionMain();
+    UpdateScroll();
+}
